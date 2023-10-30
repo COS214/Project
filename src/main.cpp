31676c874d185885623e0df39bffab4e3f601c89
Pieryvdl <pieryvanderlinde@gmail.com>
2023-10-30 23:42:53 +0200

    addiditonal changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,41 @@
 #include "Bill.h"
 #include "Tab.h"
 
-// Function to test the Bill class
 void testBill() {
-    // Create a Bill
+    ///create a new bill
     Bill bill("123", "John", 50.0, 2, 4, "TAB001", false);
 
-    // Test getTotalAmount
+    ///expect a certain amount
     double expectedTotalAmount = 50.0;
-    double actualTotalAmount = bill.getTotalAmount(bill);
+    double actualTotalAmount = bill.getTotalAmount(bill); //check if you get the expected amount back
     if (expectedTotalAmount == actualTotalAmount) {
         std::cout << "getTotalAmount test passed" << std::endl;
     } else {
         std::cout << "getTotalAmount test failed" << std::endl;
     }
 
-    // Add more test cases for other Bill methods
+    
 }
 
-// Function to test the Tab class
 void testTab() {
-    // Create a Tab
+    /// create a new tab
     Tab tab;
     BillMemento memento("123", "John", 50.0, 2, 4, "TAB001", false);
 
-    // Test addBill and getBillMemento
+    ///add bill to tab
     tab.addBill(memento, "Order1");
     BillMemento retrievedMemento = tab.getBillMemento("Order1");
 
+    ///use memento to test memento functionality
     if (retrievedMemento.getOrderID() == "123" && retrievedMemento.getCustomerID() == "John" && retrievedMemento.getTotalAmount() == 50.0) {
         std::cout << "addBill and getBillMemento test passed" << std::endl;
     } else {
         std::cout << "addBill and getBillMemento test failed" << std::endl;
     }
-
-    // Add more test cases for other Tab methods
 }
 
 int main() {
-    // Run the tests
+    ///run the tests
     testBill();
     testTab();
 
