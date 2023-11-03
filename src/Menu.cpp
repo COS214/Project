1aa5ddf8223a3a0936f0f88c90224fb02bad4e6e
aliyahlimbada <131686630+aliyahlimbada@users.noreply.github.com>
2023-11-03 15:25:42 +0200

    added menu classes

---
+++ src/Menu.cpp
@@ -0,0 +1,24 @@
+#include "Menu.h"
+
+
+void Menu::addDish(const std::string& key, Dish* dish, double cost) {
+        menuItems[key] = std::make_pair(dish, cost);
+    }
+
+std::pair<Dish*, double> Menu::getDish(const std::string& key) {
+    auto it = menuItems.find(key);
+    if (it != menuItems.end()) {
+        return it->second;
+    }
+    return std::make_pair(nullptr, 0.0); // Dish not found
+}
+
+void Menu::printMenu() {
+    std::cout << "Menu:\n";
+    for (const auto& item : menuItems) {
+        const std::string& key = item.first;
+        Dish* dish = item.second.first;
+        double cost = item.second.second;
+        std::cout << key << " - " << dish->getName() << " - Cost: $" << cost << std::endl;
+    }
+}
